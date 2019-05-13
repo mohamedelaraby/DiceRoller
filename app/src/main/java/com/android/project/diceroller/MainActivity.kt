83ceca4d2b6ae3d644de1d349d0be3cfe992fc2b
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-05-13 05:54:44 -0700

    add Dice Image

--- app/src/main/java/com/android/project/diceroller/MainActivity.kt
+++ app/src/main/java/com/android/project/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -12,26 +13,26 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     //Class instances.
-
+    private lateinit  var rollButton: Button
+    private lateinit  var  diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setTextForButton()
+
+        setupButton()
     }
 
     //Setup Roller button
-     private fun setTextForButton(){
-        val rollButton = findViewById<Button>(R.id.roll_button)
+    private fun setupButton() {
+
+        rollButton = findViewById(R.id.roll_button)
         rollButton.text = getString(R.string.roll_Button)
         //Create a Toast
 
         //Generate random number
-        rollButton.setOnClickListener{
-//            val context: Context = this
-//            val message = "Hello from dice roller"
-//            val duration: Int = Toast.LENGTH_SHORT
-//            Toast.makeText(context,message,duration).show()
+        rollButton.setOnClickListener {
+
 
             rollDice()
         }
@@ -39,14 +40,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     //Generate Random number
-    private fun rollDice(){
+    private fun rollDice() {
         //get random numbers.
         val randomInt = Random().nextInt(6) + 1
-//find the textView by Id
-        val rollText = findViewById<TextView>(R.id.result_text)
 
-        //Set the text to the textView
-        rollText.text = (randomInt.toString())
+        //Find the Dice image by Id.
+       diceImage = findViewById(R.id.dice_Image)
 
+        //Setup the image view resources for dice images.
+        val drawableResources = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResources)
     }
 }
